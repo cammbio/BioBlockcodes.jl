2d85d49614108634ea27ecfb8393b633d24ba202
fcavar <93148288+fcavar@users.noreply.github.com>
2026-02-24 11:10:24 +0100

    - hopefully last fix for docstring

--- src/InputOutputUtilities.jl
+++ src/InputOutputUtilities.jl
@@ -23,7 +23,7 @@ julia> using GCATCodes
 
 julia> codon_set = GCATCodes.LongDNA{4}.(["ATG", "TGA"]);
 
-julia> codon_set_to_str(codon_set) == "\"ATG\", \"TGA\""
+julia> codon_set_to_str(codon_set) == "\\\"ATG\\\", \\\"TGA\\\""
 true
 ```
 """
