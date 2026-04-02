02c950f3a6b93a9e55457dc736712d6173729633
Markus Gumbel <m.gumbel@hs-mannheim.de>
2026-04-02 11:36:47 +0200

    Delegated some functions to BioSequences.

--- src/CodonUtilities.jl
+++ src/CodonUtilities.jl
@@ -75,12 +75,7 @@ function left_shift_codon(codon::LongDNA{4}, shift_by::Int)
     # validate codon
     _validate_codon(codon)
 
-    # limit shift_by to length of codon
-    shift_by = mod(shift_by, length(codon))
-    shift_by == 0 && return copy(codon)
-    # cut of first shift_by characters and append them to the end
-    shifted_codon = codon[(shift_by + 1):end] * codon[1:shift_by]
-    return shifted_codon
+    return circshift(codon; k = shift_by)
 end
 
 
@@ -115,21 +110,12 @@ julia> left_shift_codon_set(codon_set, 1)
  GAT
 ```
 """
-function left_shift_codon_set(codon_set::Vector{LongDNA{4}}, shift_by::Int)
+function left_shift_codon_set(codon_set::Vector{T}, shift_by::Int) where {T <: LongDNA{4}}
     # do not allow negative shift_by
     shift_by < 0 && throw(ArgumentError("shift_by must be non-negative, got shift_by = $shift_by."))
     # validate codon_set
     _validate_codon_set(codon_set)
-
-    # limit shift_by to length of codon
-    shift_by = mod(shift_by, length(codon_set[1]))
-    shift_by == 0 && return copy(codon_set)
-    # shift every codon from codon_set
-    shifted_codon_set = Vector{LongDNA{4}}(undef, length(codon_set))
-    for (idx, codon) in enumerate(codon_set)
-        shifted_codon_set[idx] = left_shift_codon(codon, shift_by)
-    end
-    return shifted_codon_set
+    circshift.(codon_set; k = shift_by)
 end
 
 
@@ -137,7 +123,7 @@ function _get_comp_base(base::DNA)
     # do not allow bases not in ALLOWED_BASES_DNA
     !(base in ALLOWED_BASES_DNA) && throw(ArgumentError("invalid base \"$base\": must be A, C, G or T."))
 
-    return BASE_COMPLEMENT[base]
+    return BioSequences.complement(base)
 end
 
 
@@ -146,8 +132,7 @@ function _get_comp_codon(codon::LongDNA{4})
     _validate_codon(codon)
 
     # get complemented codon
-    comp_codon = LongDNA{4}(_get_comp_base.(codon))
-    return comp_codon
+    return BioSequences.complement(codon)
 end
 
 
@@ -156,13 +141,7 @@ function _get_comp_codon_set(codon_set::Vector{LongDNA{4}})
     _validate_codon_set(codon_set)
 
     # build complemented codon set
-    comp_codon_set = Vector{LongDNA{4}}()
-    for codon in codon_set
-        # add the complemented codon to the complemented_codons set
-        push!(comp_codon_set, _get_comp_codon(codon))
-    end
-
-    return comp_codon_set
+    return BioSequences.complement.(codon_set)
 end
 
 
@@ -171,8 +150,7 @@ function _get_rev_codon(codon::LongDNA{4})
     _validate_codon(codon)
 
     # get reversed codon
-    rev_codon = codon[end:-1:1]
-    return rev_codon
+    return BioSequences.reverse(codon)
 end
 
 
@@ -181,13 +159,7 @@ function _get_rev_codon_set(codon_set::Vector{LongDNA{4}})
     _validate_codon_set(codon_set)
 
     # build reversed codon set
-    rev_codon_set = Vector{LongDNA{4}}()
-    for codon in codon_set
-        # add the reversed complemented codon to the reversed_codons set
-        push!(rev_codon_set, _get_rev_codon(codon))
-    end
-
-    return rev_codon_set
+    return BioSequences.reverse.(codon_set)
 end
 
 
--- test/codon_graph_analysis_test.jl
+++ test/codon_graph_analysis_test.jl
@@ -1,3 +1,4 @@
+using BioCodes
 using BioSequences
 using BioBlockcodes
 using Graphs
